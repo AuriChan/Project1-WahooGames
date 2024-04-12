d3bc939547712beb2338a4e943ba09a8e8d1583a
kiwiipaw <kiwiipaw@gmail.com>
2024-04-12 10:53:14 +0200

    Complete game, scene, tilemap
    make a menu window and play window

--- VampireKiller/Source/TileMap.cpp
+++ VampireKiller/Source/TileMap.cpp
@@ -39,3 +39,54 @@ AppStatus TileMap::Initialise()
 	}
 	return AppStatus::OK;
 }
+
+AppStatus TileMap::Load(int data[], int w, int h)
+{
+	int size = w * h;
+
+	width = w;
+	height = h;
+	map_y = WINDOW_HEIGHT - h * TILE_SIZE;
+
+	if (map != nullptr)	delete[] map;
+
+	map = new Tile[size];
+	if (map == nullptr)
+	{
+		LOG("Failed to allocate memory for tile map");
+		return AppStatus::ERROR;
+	}
+	memcpy(map, data, size * sizeof(int));
+
+	return AppStatus::OK;
+}
+void TileMap::Update()
+{
+}
+void TileMap::Render()
+{
+	Tile tile;
+	Rectangle rc;
+	Vector2 pos;
+
+	for (int i = 0; i < height; ++i)
+	{
+		for (int j = 0; j < width; ++j)
+		{
+			tile = map[i * width + j];
+			if (tile != Tile::AIR)
+			{
+				pos.x = j * TILE_SIZE;
+				pos.y = map_y + i * TILE_SIZE;
+
+				rc = dict_rect[(int)tile];
+				DrawTextureRec(img_tiles, rc, pos, WHITE);
+			}
+		}
+	}
+}
+void TileMap::Release()
+{
+	UnloadTexture(img_tiles);
+	dict_rect.clear();
+}
