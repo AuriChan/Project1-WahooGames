c4b536c9e0d0535ddb98fb96fd836db538940029
kiwiipaw <157851285+kiwiipaw@users.noreply.github.com>
2024-04-20 22:00:46 +0200

    Change heart sprite

--- VampireKiller/Source/Object.cpp
+++ VampireKiller/Source/Object.cpp
@@ -12,7 +12,7 @@ Object::Object(const Point& p, ObjectType t) : Entity(p, OBJECT_PHYSICAL_SIZE, O
 	{
 	case ObjectType::APPLE: rc = { 4 * n, 3 * n, n, n }; break;
 	case ObjectType::CHILI: rc = { 5 * n, 3 * n, n, n }; break;
-	case ObjectType::HEART: rc = { 4 * n, 1 * n, n, n }; break;
+	case ObjectType::HEART: rc = { 3 * n, 1 * n, n, n }; break;
 	case ObjectType::THE_RING: rc = { 3 * n, 2 * n, n, n }; break;
 	/*case ObjectType::FIRE: rc = { 5 * n, 3 * n, n, n }; break;*/
 
